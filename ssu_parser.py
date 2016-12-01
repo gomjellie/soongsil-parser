c9ca704946bbd55938630713524f82a49b61c94b
gomjellie <gomjellie@naver.com>
2016-12-01 22:40:38 +0900

    ssu_parser 작동확인

--- ssu_parser.py
+++ ssu_parser.py
@@ -6,6 +6,7 @@ class ssu_parser(object):
     def __init__(self):
         self.url_base = 'http://www.ssu.ac.kr/web/kor/plaza_d_01'
         self.noti_outline = 1
+        self.department = 3
         self.date = 4
         self.visit_count = 5
 
@@ -13,25 +14,24 @@ class ssu_parser(object):
         self.r = requests.get(self.url_base)
         self.soup = bs(self.r.text, 'html.parser')
 
-        self.noti_list = [noti.text for noti in self.soup.find('div', {'id' : 'bbslist'}).find_all('span', {'class' : 'subject'})]
-
-        self.date_list = [re.search("[0-9]{4}.[0-9]{2}.[0-9]{2}",tr.text).group() for tr in
-                self.soup.find('div', {'id' : 'bbslist'}).find_all('div', {'class' : 'info'})]
+        self.link = [tr.get('href') for tr in self.soup.tbody.find_all('a', href=True)]
+        self.noti_list = [tr.find_all('td')[self.noti_outline].text for tr in self.soup.tbody.find_all("tr")]
+        self.dept_list = [tr.find_all('td')[self.department].text for tr in self.soup.tbody.find_all("tr")]
+        self.date_list = [tr.find_all('td')[self.date].text for tr in self.soup.tbody.find_all("tr")]
         #self.visit_count_list = [tr.find_all('td')[self.visit_count].text for tr in self.soup.tbody.find_all("tr")]
-        self.link = [tr.get('href') for tr in soup.tbody.find_all('a', href=True)]
 
         self.my_tb = zip(self.dept_list, self.noti_list, self.date_list, self.link)
 
     def get_notification(self):
         self.ret = ''
-        refresh_notificationt()
+        self.refresh_notificationt()
         
         for dept, noti, date, link in self.my_tb:
-            ret += '[{department}] <a href={link}>{notification}</a> \n스시소 공지:{date}\n'.format(\
+            self.ret += '[{department}] <a href={link}>{notification}</a> \n숭실대 공지:{date}\n'.format(\
                     department = dept,
                     notification = noti,
                     date = date,
                     link = link)
-            ret.replace('\t', '')
-        return ret
+            self.ret.replace('\t', '')
+        return self.ret
 
--- susiso.py
+++ susiso.py
@@ -13,10 +13,13 @@ class susiso_parser(object):
     def refresh_notificationt(self):
         self.r = requests.get(self.url_base)
         self.soup = bs(self.r.text, 'html.parser')
+        
+        self.noti_list = [noti.text for noti in self.soup.find('div', {'id' : 'bbslist'}).find_all('span', {'class' : 'subject'})]
+        self.date_list = [re.search("[0-9]{4}.[0-9]{2}.[0-9]{2}",tr.text).group() for tr in
+                self.soup.find('div', {'id' : 'bbslist'}).find_all('div', {'class' : 'info'})]
 
-        self.noti_list = [tr.find_all('td')[self.noti_outline].text for tr in self.soup.tbody.find_all("tr")]
-        self.dept_list = [tr.find_all('td')[self.department].text for tr in self.soup.tbody.find_all("tr")]
-        self.date_list = [tr.find_all('td')[self.date].text for tr in self.soup.tbody.find_all("tr")]
+        self.link = [tr.get('href') for tr in soup.tbody.find_all('a', href=True)]
+
         #self.visit_count_list = [tr.find_all('td')[self.visit_count].text for tr in self.soup.tbody.find_all("tr")]
 
         self.my_tb = zip(self.dept_list, self.noti_list, self.date_list)
